99addb94122ca5f07c8402b0a2d06a6db7f13bb0
Mara Pislar <pislarmara589@gmail.com>
2020-07-24 14:32:50 +0000

    Add mood and timestamp to a comment.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -1,17 +1,21 @@
 /**
-* A comment has an id, text and an author. 
+* A comment has an id, text, an author and a mood.
 */
 
 package com.google.sps.data;
 
 public final class Comment {
   public final long id;
+  public final long timestamp;
   public final String text;
   public final String author;
+  public final String mood;
 
-  public Comment(long id, String text, String author) {
+  public Comment(long id, String text, String author, String mood, long timestamp) {
     this.id = id;
     this.text = text;
     this.author = author;
+    this.mood = mood;
+    this.timestamp = timestamp;
   }
 }
