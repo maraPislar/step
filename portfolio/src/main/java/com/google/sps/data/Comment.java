ebae5abc35013a1feae25682c5fa826fc2f82f20
Mara Pislar <pislarmara589@gmail.com>
2020-07-27 11:27:01 +0000

    Add getters and modity the type of the timestamp into Long for sorting it.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -6,16 +6,24 @@ package com.google.sps.data;
 
 public final class Comment {
   public final long id;
-  public final long timestamp;
+  public final Long timestamp;
   public final String text;
   public final String author;
   public final String mood;
 
-  public Comment(long id, String text, String author, String mood, long timestamp) {
+  public Comment(long id, String text, String author, String mood, Long timestamp) {
     this.id = id;
     this.text = text;
     this.author = author;
     this.mood = mood;
     this.timestamp = timestamp;
+  }
+
+  public Long getTimestamp() {
+      return this.timestamp;
+  }
+
+  public Integer getTextLength() {
+      return (Integer) this.text.length();
   }
 }
